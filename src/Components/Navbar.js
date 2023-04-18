ba73cab415f53e81f4c9aefdbd754babbb7973d0
Gurpreet Singh <91670399+gsinghg19@users.noreply.github.com>
2023-04-18 17:06:36 +0100

    added title name for movies to render

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -23,7 +23,7 @@ function Navbar() {
         <img
           className="nav_logo"
           src="https://th.bing.com/th/id/R.715e69176d3cec74504b3ae00f2676c6?rik=vGDJl%2bfFL64qmw&riu=http%3a%2f%2fwww.pngall.com%2fwp-content%2fuploads%2f4%2fNetflix-Logo-HD.png&ehk=aWIF5J5TR6tD4IrfNFhXE0dNVq%2f6n%2f6OViRGEsRS%2bNg%3d&risl=&pid=ImgRaw&r=0"
-          alt=""
+          alt="company logo"
         />
         <img
           className="nav_avatar"
