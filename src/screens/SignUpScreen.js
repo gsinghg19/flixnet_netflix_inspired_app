a8835a684e2cae90c484a295acf00842bc822f52
Gurpreet Singh <91670399+gsinghg19@users.noreply.github.com>
2023-04-19 17:02:23 +0100

    fixed styling issues for sign up pages and login in screen

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,7 +1,17 @@
 import React from "react";
+import "./SignUpScreen.css";
 
 function SignUpScreen() {
-  return <div>SignUpScreen</div>;
+  return (
+    <div className="signUpScreen">
+      <form>
+        <h1 className="signUpScreen_Header">Sign up here</h1>
+        <input placeholder="Email Address" type="email" />
+        <input placeholder="Password" type="Password" />
+        <button type="submit">Sign In</button>
+      </form>
+    </div>
+  );
 }
 
 export default SignUpScreen;
