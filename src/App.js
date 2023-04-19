4154707d543c3e3f7cb4a1746c78f926b6d3ca18
Gurpreet Singh <91670399+gsinghg19@users.noreply.github.com>
2023-04-19 11:58:47 +0100

    fix: added inital routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import React from "react";
 import HomeScreen from "./HomeScreen";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <HomeScreen />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        {/*add route for login page here */}
+        {/*add route for checkout pay page with stripe api here*/}
+        {/*add route for my fav movies page here*/}
+        {/*add route for watch later page here*/}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
