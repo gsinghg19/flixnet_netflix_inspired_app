e81eef16ea6f1115d1b3221cf211b50d9f8f6e5a
Gurpreet Singh <91670399+gsinghg19@users.noreply.github.com>
2023-04-20 11:55:06 +0100

    fix 4.0: fixed refreshing of the page when the signin and logini buttons are clicked.

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -2,18 +2,28 @@ import React from "react";
 import "./SignUpScreen.css";
 
 function SignUpScreen() {
+  const register = (e) => {
+    e.preventDefault();
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="signUpScreen">
       <form>
         <h1 className="signUpScreen_Header">Sign up here</h1>
         <input placeholder="Email Address" type="email" />
         <input placeholder="Password" type="Password" />
-        <button className="signUpButton_Submit" type="submit">
+        <button className="signUpButton_Submit" type="submit" onClick={signIn}>
           Sign In
         </button>
         <h4>
           <span className="signUpScreen_gray">New to netflix?</span>
-          <span className="signUpScreen_link">Sign up now!</span>
+          <span className="signUpScreen_link" onClick={register}>
+            Sign up now!
+          </span>
         </h4>
       </form>
     </div>
