e803a6542cbc757564a0441a7e523e76b6fecf54
Gurpreet Singh <91670399+gsinghg19@users.noreply.github.com>
2023-04-21 14:17:50 +0100

    feature 6.2: added user persistace for their logged in session.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import "./App.css";
-import React from "react";
+import React, { useEffect } from "react";
 import HomeScreen from "./screens/HomeScreen";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen";
+import { auth } from "./firebase_handler";
 
 function App() {
-  // const user = {
-  //   name: "tester",
-  // };
   const user = null;
+
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        //Logged in state
+        console.log(userAuth);
+      } else {
+        //Logged out state
+      }
+    });
+  }, []);
 
   return (
     <div className="app">
@@ -18,11 +27,6 @@ function App() {
         ) : (
           <Routes>
             <Route exact path="/" element={<HomeScreen />} />
-            {/*add route for login page here */}
-            {/*add sign up page here*/}
-            {/*add route for checkout pay page with stripe api here*/}
-            {/*add route for my fav movies page here*/}
-            {/*add route for watch later page here*/}
           </Routes>
         )}
       </BrowserRouter>
