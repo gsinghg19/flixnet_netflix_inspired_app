27949dd36e1217f8599e4ce4c340aff010ee22df
Gurpreet Singh <91670399+gsinghg19@users.noreply.github.com>
2023-04-24 11:08:25 +0100

    fix : added navigation for the netflix logo, to navigate back to the homescreen.

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -26,6 +26,7 @@ function Navbar() {
           className="nav_logo"
           src="https://th.bing.com/th/id/R.715e69176d3cec74504b3ae00f2676c6?rik=vGDJl%2bfFL64qmw&riu=http%3a%2f%2fwww.pngall.com%2fwp-content%2fuploads%2f4%2fNetflix-Logo-HD.png&ehk=aWIF5J5TR6tD4IrfNFhXE0dNVq%2f6n%2f6OViRGEsRS%2bNg%3d&risl=&pid=ImgRaw&r=0"
           alt="company logo"
+          onClick={() => navigate("/")}
         />
         <img
           className="nav_avatar"
