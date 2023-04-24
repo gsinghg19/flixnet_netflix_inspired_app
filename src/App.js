271f75725953cfa2eb975a5bc7f7d4798f4ca60c
Gurpreet Singh <91670399+gsinghg19@users.noreply.github.com>
2023-04-24 10:39:08 +0100

    fix: useSelecter added to code. Now login in screen  will check for logged in users and redirect to the app, only when logged in with correct credentials.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen";
 import { auth } from "./firebase_handler";
-import { useDispatch } from "react-redux";
-import { login, logout } from "./features/counter/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/counter/userSlice";
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
   const dispatch = useDispatch();
 
   useEffect(() => {
